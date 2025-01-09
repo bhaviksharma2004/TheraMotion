b17b320e510f20e1a75e9f3c0a5ab0e1aca5e683
Bhavik Sharma <bhaviksharma13dec@gmail.com>
2025-01-09 20:29:03 +0530

    created routes.jsx and connected some components

---
+++ FrontEnd/src/routes.jsx
@@ -0,0 +1,99 @@
+import Layout from "./layout";
+import SignUp from "./pages/authentication/Signup.jsx";
+import SignIn from "./pages/authentication/Signin.jsx";
+import JoinTeam from "./pages/join team/JoinTeam.jsx";
+import MainPage from "./pages/main page/MainPage.jsx";
+import MeetTeam from "./pages/meet team/MeetTeam.jsx";
+import Services from "./pages/services/Service.jsx";
+import Blog from "./pages/blog/Blog.jsx";
+import UserProfile from "./pages/user profile/UserProfile.jsx"
+import MeetTeamLayout from "./pages/meet team/MeetTeamLayout.jsx";
+import PersonDetail from "./components/PersonDetails/PersonDetails.jsx";
+import BookAppt from "./pages//book appointment/BookAppt.jsx";
+import Personselect from "./components/Personselect/Personselect";
+import Calendar from "./components/Calender/Calendar";
+import Finalform from "./pages/forms/Finalform.jsx";
+import OrderDetail from "./pages/book appointment/OrderDetail.jsx";
+import PersonSelectLayout from "./pages/book appointment/PersonSelectLayout.jsx";
+
+export const routes = [
+    {
+        path: "/",
+        element: <Layout />,
+        children: [
+            {
+                path: "/",
+                element: <MainPage />,
+            },
+            {
+                path: "/signup",
+                element: <SignUp />,
+            },
+            {
+                path: "/signin",
+                element: <SignIn />,
+            },
+            {
+                element: <ProtectedRoute />,
+                children: [
+                    {
+                        path: "/meet-team",
+                        element: <MeetTeamLayout />,
+                        children: [
+                            {
+                                index: true,
+                                element: <MeetTeam />,
+                            },
+                            {
+                                path: ":personId",
+                                element: <PersonDetail />,
+                            },
+                        ],
+                    },
+                    {
+                        path: "/join-team",
+                        element: <JoinTeam />,
+                    },
+                    {
+                        path: "/services",
+                        element: <Services />,
+                    },
+                    {
+                        path: "/blog",
+                        element: <Blog />,
+                    },
+                    {
+                        path: "/profile",
+                        element: <UserProfile />,
+                    },
+                    {
+                        path: "/book-appointment",
+                        element: <BookAppt />,
+                    },
+                    {
+                        path: "/person",
+                        element: <PersonSelectLayout />,
+                        children: [
+                            {
+                                index: true,
+                                element: <Personselect />,
+                            },
+                            {
+                                path: "calendar",
+                                element: <Calendar />,
+                            },
+                        ],
+                    },
+                    {
+                        path: "/form",
+                        element: <Finalform />,
+                    },
+                    {
+                        path: "/order",
+                        element: <OrderDetail />,
+                    },
+                ],
+            },
+        ],
+    },
+];
