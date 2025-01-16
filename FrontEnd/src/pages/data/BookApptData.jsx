e68935343712774f63fe67b17d380399d0b2d8b1
Bhavik Sharma <bhaviksharma13dec@gmail.com>
2025-01-16 17:06:17 +0530

    Created sample data for 3 pages

---
+++ FrontEnd/src/pages/data/BookApptData.jsx
@@ -0,0 +1,92 @@
+const BookApptData1 = [
+    {
+        name: "Exercise Physiology",
+        arr: [
+            {
+                hdata: "Double Exercise Physiology Consultation",
+                time: "60 min",
+            },
+            {
+                hdata: "Extended Exercise Physiology Consultation",
+                time: "60 min",
+            },
+            {
+                hdata: "Initial Exercise Physiology Consultation",
+                time: "30 min",
+            },
+        ],
+    },
+    {
+        name: "Physiotheraphy",
+        arr: [
+            {
+                hdata: "Initial Physiotheraphy Consultation",
+                time: "60 min",
+            },
+            {
+                hdata: "Subsequent Physiotheraphy Consultation",
+                time: "60 min",
+            },
+        ],
+    },
+    {
+        name: "Myotheraphy",
+        arr: [
+            {
+                hdata: "Initial Myotheraphy Consultation",
+                time: "60 min",
+            },
+            {
+                hdata: "Extended Myotheraphy Consultation",
+                time: "60 min",
+            },
+            {
+                hdata: "Subsequent Myotheraphy Consultation",
+                time: "30 min",
+            },
+        ],
+    },
+];
+
+const BookApptData2 = [
+    {
+        name: "Osteopathy",
+        arr: [
+            {
+                hdata: "Initial Osteopathy Consultation",
+                time: "60 min",
+            },
+            {
+                hdata: "Extended Osteopathy Consultation",
+                time: "60 min",
+            },
+        ],
+    },
+    {
+        name: "Classes",
+        arr: [
+            {
+                hdata: "Double Exercise Physiology Consultation",
+                time: "60 min",
+            },
+            {
+                hdata: "Single Exercise Physiology Consultation",
+                time: "30 min",
+            },
+        ],
+    },
+    {
+        name: "Pilates Classes",
+        arr: [
+            {
+                hdata: "Double Exercise Physiology Consultation",
+                time: "60 min",
+            },
+            {
+                hdata: "Single Exercise Physiology Consultation",
+                time: "30 min",
+            },
+        ],
+    },
+];
+export { BookApptData1, BookApptData2 };
---
+++ FrontEnd/src/pages/data/personData.jsx
@@ -0,0 +1,30 @@
+const pdata = [
+    {
+        name: "Jarrah McArthur",
+        speciality: "Exercise Physiologist",
+        interest: [
+            "Ballet/Dance Rehabilition",
+            "Women's Health",
+            "Low back and neck pain",
+        ],
+    },
+    {
+        name: "Holly Eckert",
+        speciality: "Exercise Physiologist",
+        interest: [
+            "Paediatrics",
+            "Pre and Post natal & return to exercise",
+            "Disability",
+        ],
+    },
+    {
+        name: "Rohan Steger",
+        speciality: "Exercise Physiologist",
+        interest: [
+            "Lower Back and sport injury",
+            "Falls Prevention,balance and health ageing",
+            "Disability",
+        ],
+    },
+];
+export default pdata;
