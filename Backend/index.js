781f3f50160f313a584353e90d8b7ca8a7b70c9f
Bhavik Sharma <bhaviksharma13dec@gmail.com>
2025-01-18 12:07:41 +0530

    Created running server

--- Backend/index.js
+++ Backend/index.js
@@ -1,16 +1,35 @@
-import express from "express"
-import connectDB from "./config/db.js"
+import express from "express";
+import userRoutes from "./routes/user.js";
+import { connectDB } from "./config/db.js";
 import dotenv from "dotenv";
+import appointmentRoutes from "./routes/appointment.js";
+import Counter from "./utils/counter.js";
+import path from "path";
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
 
 dotenv.config();
 
 const app = express();
 const port = "8081";
 
-// Generic Middlewares
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const initializeCounter = async () => {
+    try {
+        const counter = await Counter.findById("bookingId");
+        if (!counter) {
+            await Counter.create({ _id: "bookingId", sequenceValue: 1 });
+            console.log("Counter initialized for bookingId.");
+        }
+    } catch (error) {
+        console.error("Error initializing counter:", error);
+    }
+};
+
 app.use(express.json());
 
-// Resolving CORS Policy
 app.use("*", (req, res, next) => {
     res.setHeader("Access-Control-Allow-Origin", "*");
     res.setHeader("Access-Control-Allow-Headers", "*");
@@ -21,18 +40,28 @@ app.use("*", (req, res, next) => {
     next();
 });
 
+app.get("/", (req, res) => {
+    res.send("hello world again");
+});
+app.use("/user", userRoutes);
+app.use("/appointment", appointmentRoutes);
 
-// Custom Middlewares
+app.use(express.static(path.join(__dirname, "../FrontEnd/dist")));
 
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../FrontEnd/dist/index.html"));
+});
 
+connectDB()
+    .then(() => {
+        // Initialize counter after DB connection
+        initializeCounter();
 
-// Call to connect database
-connectDB().
-    then(() => {
+        // Start the server
         app.listen(port, () => {
-            console.log("Server running on port: ", port);
+            console.log("The server is running on port: ", port);
         });
     })
-    .catch((err) => {
-        console.log("Database Connection failed", err);
-    })
+    .catch((error) => {
+        console.error("Database connection failed:", error);
+    });
